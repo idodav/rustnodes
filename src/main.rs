8671d76eb466db711cb0afcf72b279387093c74a
Ido David <kaki>
2021-10-31 22:38:37 +0200

    added http listener

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod engine;
 use engine::nodes::*;
+use std::fs;
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let c = Nodes::Debug(Debug {
@@ -22,4 +26,29 @@ fn main() {
     });
 
     a.to_inject().inject();
+
+    // Listen for incoming TCP connections on localhost port 7878
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    // Block forever, handling each request that arrives at this IP address
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    // Read the first 1024 bytes of data from the stream
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    let received = std::str::from_utf8(&buffer).expect("valid utf8");
+    let mut headers = [httparse::EMPTY_HEADER; 64];
+    let mut req = httparse::Request::new(&mut headers);
+    req.parse(&buffer);
+    println!("{} {}", req.method.unwrap(),req.path.unwrap());
+    
+    let response = "HTTP/1.1 200 OK\r\n\r\n as asjkasjh".to_string();
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
